c31b9d85d74fad777d89f4cda6a12bf15e4df29b
sean-leishman <leishmansean@gmail.com>
2023-10-16 01:01:49 +0100

    Update model architecture. Remove final activation layer and add a dropout layer with an additional hidden layer

--- bespoketart/model.py
+++ bespoketart/model.py
@@ -9,31 +9,52 @@ class DistilledBert(torch.nn.Module):
                  pretrained_model_name="distilbert-base-uncased",
                  bert_finetuning=True, num_labels=1, config=None):
         super(DistilledBert, self).__init__()
+        self.logger = logging.getLogger(__name__)
 
-        self.tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name)
+        self.tokenizer = self.init_tokenizer(
+            AutoTokenizer.from_pretrained(pretrained_model_name))
 
         self.bert = DistilBertModel.from_pretrained(pretrained_model_name)
         self.bert.to(config.device)
-        self.logger = logging.getLogger(__name__)
 
         if not bert_finetuning:
             self.logger.info('model: bert parameters frozen')
             for param in self.bert.parameters():
                 param.requires_grad = False
 
-        self.classifier = torch.nn.Linear(
-            self.bert.config.hidden_size, num_labels)
+        self.dropout = torch.nn.Dropout(p=0.1)
+        self.fc1 = torch.nn.Linear(self.bert.config.hidden_size, 20)
+        self.relu = torch.nn.ReLU()
+
+        self.output = torch.nn.Linear(
+            20, num_labels)
 
         self.output_activation = torch.nn.Sigmoid()
 
     def forward(self, input_ids, attention_mask=None, token_type_ids=None):
-        output = self.bert(
+        bert_output = self.bert(
             input_ids, attention_mask=attention_mask)
-        hidden_state = output[0]
-        pooled_output = hidden_state[:, 0]
 
-        final_output = self.classifier(pooled_output)
-        return self.output_activation(final_output)
+        mean_pooled = torch.mean(bert_output.last_hidden_state, dim=1)
+        x = self.dropout(mean_pooled)
+        x = self.fc1(x)
+        x = self.relu(x)
+
+        final_output = self.output(x)
+        return final_output
+
+    def get_probability(self, logits):
+        return self.output_activation(logits)
+
+    def init_tokenizer(self, tokenizer):
+        num_added_token = tokenizer.add_special_tokens(
+            {'additional_special_tokens': ['[EOT]']}
+        )
+
+        self.logger.info(
+            f"model: add {tokenizer.all_special_tokens} token/s")
+
+        return tokenizer
 
     def get_tokenizer(self):
         return self.tokenizer
