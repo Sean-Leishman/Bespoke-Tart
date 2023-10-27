09d2eca0d7ff3d2983961059bfc20ad670152dc5
Sean Leishman <leishmansean@gmail.com>
2023-10-27 14:16:27 +0100

    Update wget download edacc script

--- data/edacc/edacc.wget.sh
+++ data/edacc/edacc.wget.sh
@@ -2,9 +2,10 @@
 
 wget https://datashare.ed.ac.uk/download/DS_10283_4836.zip
 
-unzip -t DS_10283_4836.zip
+unzip  DS_10283_4836.zip
+tar -xzvf data/edacc_v1.0.tar.gz 
+
 rm DS_10283_4836.zip 
+rm data/edacc_v1.0.tar.gz
 
-mv DS_10283_4836/* . 
 
-rm -rf D_S10283_4836
