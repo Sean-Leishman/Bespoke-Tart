08442807077e2beecc6d41cf79f1276c65c38a1f
sean-leishman <leishmansean@gmail.com>
2023-10-28 18:49:00 +0100

    Update setup of package

---
+++ bespoketart/__init__.py
@@ -0,0 +1,1 @@
+from bespoketart.model import Bert
--- setup.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-from setuptools import setup, find_packages
-
-
-setup(name='bespoketart', version='0.01', packages=['bespoketart'])
