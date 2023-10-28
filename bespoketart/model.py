d85ea29a2a65442d5f5bdfdd88c062bb981a7200
sean-leishman <leishmansean@gmail.com>
2023-10-28 13:11:14 +0100

    Updating model architecture

--- bespoketart/model.py
+++ bespoketart/model.py
@@ -75,17 +75,39 @@ class Bert(torch.nn.Module):
             for param in self.bert.parameters():
                 param.requires_grad = False
 
-        self.classifier = torch.nn.Linear(
-            self.bert.config.hidden_size, num_labels)
+        self.dropout = torch.nn.Dropout(p=0.1)
+        self.fc1 = torch.nn.Linear(self.bert.config.hidden_size, 20)
+        self.relu = torch.nn.ReLU()
+
+        self.output = torch.nn.Linear(
+            20, num_labels)
+
         self.output_activation = torch.nn.Sigmoid()
 
     def forward(self, input_ids, attention_mask=None, token_type_ids=None):
-        output = self.bert(
-            input_ids, attention_mask=attention_mask, token_type_ids=None)
-        hidden_state = output[0]
-        pooled_output = hidden_state[:, 0]
+        bert_output = self.bert(
+            input_ids, attention_mask=attention_mask)
 
-        return self.classifier(pooled_output)
+        # mean_pooled = torch.mean(bert_output.last_hidden_state, dim=1)
+        x = bert_output[0]
+        x = self.fc1(x)
+        x = self.relu(x)
+
+        final_output = self.output(x)
+        return final_output
+
+    def get_probability(self, logits):
+        return self.output_activation(logits)
+
+    def init_tokenizer(self, tokenizer):
+        num_added_token = tokenizer.add_special_tokens(
+            {'additional_special_tokens': ['[EOT]']}
+        )
+
+        self.logger.info(
+            f"model: add {tokenizer.all_special_tokens} token/s")
+
+        return tokenizer
 
     def get_tokenizer(self):
         return self.tokenizer
