5f446d7423570aac8b99cd99b995937d77abcdd1
sean-leishman <leishmansean@gmail.com>
2023-10-30 22:30:22 +0000

    Update README and data analysis

--- script_train.bash
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python bespoketart/train.py --description "" --learning-rate 0.0003 --batch-size 32  --output-window 5 --loss-weight 1 --overwrite "true"
---
+++ tart_script_train.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python bespoketart/train.py --description "" --learning-rate 0.003 --batch-size 64  --output-window 5 --loss-weight 5
