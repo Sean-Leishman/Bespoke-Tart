fbb560cfb6d87b0c593a0f9e69132cb70c688a06
sean-leishman <leishmansean@gmail.com>
2023-11-07 23:34:12 +0000

    Update model README

--- tart_script_train.bash
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python bespoketart/train.py --description "using a transformer after BERT embedding. predicting tokens" --learning-rate 0.003 --batch-size 8  --output-window 5 --loss-weight 5
