661bc8db2c6917f32a5a1f73dd5f6d940d792b37
sean-leishman <leishmansean@gmail.com>
2023-11-07 23:31:35 +0000

    Add scripts for both types of BERT

---
+++ gtart_script_train.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python gbespoketart/train.py --description "using a transformer after BERT embedding. predicting tokens" --learning-rate 0.003 --batch-size 8  --output-window 5 --loss-weight 5
