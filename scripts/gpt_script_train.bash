587eceac264f14ed3433e4a41d4e60e5a8c54707
sean-leishman <leishmansean@gmail.com>
2023-11-21 00:55:16 +0000

    Additional training runs

--- scripts/gpt_script_train.bash
+++ scripts/gpt_script_train.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # shellcheck disable=SC1101
-python gptonly/train.py --description "gpt" \
---learning-rate 0.00001 --batch-size 6  --output-window 5 --bert-pretraining "gpt2" \
+python gptonly/train.py --description "gpt. increase lr to 1e-5. Change split to ~" \
+--learning-rate 0.00001 --batch-size 4  --output-window 5 --bert-pretraining "gpt2" \
 --bert-finetuning "true" --overwrite "false"
