479e6db0a14a86dba8c7149a46c5db1ca38ecd6f
sean-leishman <leishmansean@gmail.com>
2023-12-11 21:55:04 +0000

    Add fisher

--- scripts/gpt_script_train.bash
+++ scripts/gpt_script_train.bash
@@ -3,4 +3,4 @@
 # shellcheck disable=SC1101
 python gptonly/train.py --description "gpt. increase lr to 1e-5. Change split to ~" \
 --learning-rate 0.00002 --batch-size 4  --pretrained "gpt2" \
---finetune --cuda --datasets "switchboard"
+--finetune --cuda --datasets "switchboard" "fisher"
